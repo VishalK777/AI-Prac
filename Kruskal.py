bde04e5cde9e344ee2974e6216ab7a8ff9ea5be4
Vishal Kumdale <126568877+VishalK777@users.noreply.github.com>
2023-05-30 01:09:51 +0530

    Create Kruskal.py

---
+++ Kruskal.py
@@ -0,0 +1,65 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, src, dest, weight):
+        self.graph.append([src, dest, weight])
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    def kruskal(self):
+        result = []
+        i = 0
+        e = 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i += 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            if x != y:
+                e += 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        print("Edges in the Minimum Spanning Tree:")
+        for u, v, weight in result:
+            print(f"{u} -- {v} == {weight}")
+
+# Example usage
+g = Graph(6)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 2, 2)
+g.add_edge(1, 2, 1)
+g.add_edge(1, 3, 5)
+g.add_edge(2, 3, 8)
+g.add_edge(2, 4, 10)
+g.add_edge(3, 4, 2)
+g.add_edge(3, 5, 6)
+g.add_edge(4, 5, 3)
+
+g.kruskal()
